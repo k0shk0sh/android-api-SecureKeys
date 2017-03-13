c6049410a178bc137f0fd1e5c84d8687bda5629a
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-03-12 23:08:26 -0300

    Unzip run with -q because too much verbose

--- ci/run_before_install.sh
+++ ci/run_before_install.sh
@@ -2,7 +2,7 @@ yes | sudo apt-get install unzip
 
 wget https://dl.google.com/android/repository/android-ndk-r14-linux-x86_64.zip -O ndk.zip
 
-unzip ndk.zip
+unzip -qq ndk.zip
 
 export ANDROID_NDK_HOME=$PWD/android-ndk-r14
 
